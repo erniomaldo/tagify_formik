58a2c28df77b18bd1d326a8edadfde69515f814b
Ernesto Maldonado <ernion@gmail.com>
2020-09-25 15:40:20 -0500

    First commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import '../styles/globals.css'
+import "@yaireo/tagify/dist/tagify.css"
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,36 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import { Formik, Form } from 'formik';
+import Tags from "@yaireo/tagify/dist/react.tagify";
 
-export default function Home() {
+const App = () => {
+  const validate = val => {
+    const errors = {};
+    Object.keys(val).map(field => {
+      if (val[field] === '') {
+        errors[field] = 'Required';
+      }
+    })
+    return errors;
+  }
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+    <Formik
+      initialValues={{
+        keywords: '[{"value":"a"},{"value":"b"},{"value":"c"}]'
+      }}
+      validate={e => validate(e)}
+    >
+      {({ values, setFieldValue }) => (
+        <Form style={{ width: "50%", margin: "20%" }}>
+          <Tags
+            value={values['keywords']}
+            onChange={e => {
+              setFieldValue('keywords', e.target.value)
+            }}
+          />
+          Values: {values['keywords']}
+        </Form>
+      )}
+    </Formik>
+  );
 }
+
+export default App;
